62c769dae4e809406cbdd20e07156f1326c5fb8f
valmir <valmir@gmx.de>
2019-07-01 19:52:14 +0200

    reverted date format change for gh archive

--- src/service/archiveService.py
+++ src/service/archiveService.py
@@ -6,9 +6,8 @@ class ArchiveService:
 
   def __init__(self, configService: ConfigService):
     self.baseUrl = configService.config['github']['archive-url']
-    self.dateFormat = configService.config['date']['format']
 
   def retrieveData(self, date):
-    response = requests.get(self.baseUrl + date.strftime(self.dateFormat) + '.json.gz')
+    response = requests.get(self.baseUrl + date.strftime('%Y-%m-%d-%-H') + '.json.gz')
     if response.status_code == 200:
       return gzip.decompress(response.content)
